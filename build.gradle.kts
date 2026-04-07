4580bb1209ffd0a9728c4b7ac031afd38a3b5ad5
Alex <sapphire6061@gmail.com>
2026-04-07 13:23:26 +0300

    added allure

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("io.github.bonigarcia:webdrivermanager:6.3.4")
 }
 
 tasks.test {
