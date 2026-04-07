b7e7f25d85eb6159e15c3903fc581dfb0c0168cc
Alex <sapphire6061@gmail.com>
2026-04-07 16:21:23 +0300

    test

--- src/test/java/setup/TestBase.java
+++ src/test/java/setup/TestBase.java
@@ -8,33 +8,31 @@
 
   import com.codeborne.selenide.Configuration;
   import com.codeborne.selenide.logevents.SelenideLogger;
-  import io.github.bonigarcia.wdm.WebDriverManager;
   import io.qameta.allure.selenide.AllureSelenide;
   import org.junit.jupiter.api.AfterAll;
   import org.junit.jupiter.api.BeforeAll;
   import org.junit.jupiter.api.BeforeEach;
-  import org.openqa.selenium.WebDriverException;
   import org.openqa.selenium.chrome.ChromeOptions;
 
   import static com.codeborne.selenide.Selenide.closeWebDriver;
 
   public class TestBase {
+
     @BeforeAll
-    public static void setup() {
-
+    public static void setup() {Ф
       ChromeOptions options = new ChromeOptions();
-      options.addArguments(
-        "--no-sandbox",
-        "--disable-dev-shm-usage"
-      );
+      options.addArguments("--remote-allow-origins=*");
 
-      Configuration.remote = "https://selenoid.autotests.cloud/wd/hub";
       Configuration.browser = "chrome";
-      Configuration.browserVersion = "120.0";
-
+      Configuration.reportsFolder = "target/selenide-reports";
+      Configuration.timeout = 5000;
       Configuration.browserCapabilities = options;
       Configuration.browserSize = "1920x1080";
-      Configuration.timeout = 10000;
+    }
+
+    @BeforeEach
+    public void logger() {
+      SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @AfterAll
@@ -42,4 +40,4 @@
       closeWebDriver();
     }
   }
-
+  
